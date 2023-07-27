9613986f7abab8b6f4d025615c3dd6f8c2021679
Matthew McCaughey <matt.mccaughey91@gmail.com>
2023-07-27 08:43:08 -0400

    Documentation update
    Added JavaDoc for ApiCalls

--- presentation/src/main/java/api/ApiCalls.java
+++ presentation/src/main/java/api/ApiCalls.java
@@ -7,10 +7,25 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import org.json.JSONObject;
 
+/**
+ * The ApiCalls class is intended to make the API calls to retrieve the returned content directly from API.Bible.
+ * @author Matthew McCaughey
+ *
+ */
 public class ApiCalls {
 	
+	/**
+	 * The API Key for API.Bible
+	 */
 	private static final String API_KEY = "db1951b2ccc58f601dbf816e7c27723e";
 	
+	/**
+	 * Gets the Json response from bible-api.com that contains the specific bible verse text within it.
+	 * @param bookId The book ID of the book that the verse is in
+	 * @param chapter The chapter that the verse is in
+	 * @param verse The verse number to get the text from
+	 * @return The bible verses Json response
+	 */
 	protected static JSONObject getVerseJson(String bookId, int chapter, int verse) {
     	try {
     		URL url = new URL("https://bible-api.com/" + bookId + " " + Integer.toString(chapter) + ":" + Integer.toString(verse) + "?translation=kjv");
@@ -44,6 +59,11 @@ public class ApiCalls {
     	
     }
 	
+	/**
+	 * Retruns the Chapters that are contained within the specific book
+	 * @param bookId The book ID of the book that the chapters are in
+	 * @return The Json response that contains the chapters
+	 */
 	protected static JSONObject getChaptersJson(String bookId) {
     	try {
             URL url = new URL("https://api.scripture.api.bible/v1/bibles/de4e12af7f28f599-02/books/" + bookId + "/chapters");
@@ -77,6 +97,10 @@ public class ApiCalls {
         return null;
     }
     
+	/**
+	 * Returns all 66 book titles of the bible
+	 * @return The Json response containing all 66 books of the bible
+	 */
     protected static JSONObject getBooksJson() {
     	try {
             URL url = new URL("https://api.scripture.api.bible/v1/bibles/de4e12af7f28f599-02/books");
@@ -110,6 +134,12 @@ public class ApiCalls {
         return null;
     }
     
+    /**
+     * Returns all verse numbers that are contained in the specified book & chapter
+     * @param bookId The book ID of the book that the verses are in
+     * @param chapter The chapter that the verses are in
+     * @return The Json response that contains the verses
+     */
     protected static JSONObject getVersesJson(String bookId, int chapter) {
     	try {
     		URL url = new URL("https://api.scripture.api.bible/v1/bibles/de4e12af7f28f599-02/chapters/" + bookId + "." + Integer.toString(chapter));
